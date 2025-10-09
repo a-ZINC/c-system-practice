db6a90aa563b9d1a5d19a199e4cda61094358ab2
a-ZINC <azinc28@gmail.com>
2025-10-09 23:55:39 +0530

    understanding copy-on-write

--- process/fork.c
+++ process/fork.c
@@ -2,16 +2,17 @@
 #include<unistd.h>
 #include<time.h>
 
-
 int main() {
     printf("hello world \n");
+    int a = 10;
     pid_t cid = fork();
 
     if (cid == 0) {
-        printf("child \n");
+        a = 20;
+        printf("child %d\n", a);
     } else {
         sleep(5);
-        printf("parent \n");
+        printf("parent %d\n", a);
     }
 
     return 0;
